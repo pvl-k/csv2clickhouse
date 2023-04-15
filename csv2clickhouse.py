88bfd3f6a349947e24f0cbe7d29dd3b2e776df80
Pavel K <pk@1702.ru>
2023-04-15 12:21:31 +0300

    Update csv2clickhouse.py

--- csv2clickhouse.py
+++ csv2clickhouse.py
@@ -3,7 +3,7 @@ import pandas as pd
 import clickhouse_connect
 
 dirpath = '/Users/pk/Library/CloudStorage/OneDrive-Личная/GitHub/csv2clickhouse/'   # absolute path to dir with csv-files and credentials
-database_name = 'testdb2'                                                           # name of database
+database_name = 'testdb7'                                                           # name of database
 database_port = '8443'                                                              # ClickHouse port
 database_secure = True                                                              # ClickHouse secure conection setting
 action_flag = 'APPEND'                                                              # Action if data exists: APPEND for append or EMPTY for cleaning data before insert 
@@ -21,12 +21,12 @@ with open(dirpath + 'db_hostname.txt', 'r', encoding='utf-8') as fp:
     database_host = fp.read().rstrip()
 
 
-def csv2clickhouse(localcpath, dbhost, dbport, dbname, dbuser, dbpassword, dbsecure, flag):
+def csv2clickhouse(localpath, dbhost, dbport, dbname, dbuser, dbpassword, dbsecure, flag):
     client = clickhouse_connect.get_client(host=dbhost, port=dbport, 
                                            username=dbuser, password=dbpassword, 
                                            secure=dbsecure)
     
-    fileslist = [filename.rsplit('/', 1)[1] for filename in glob.glob(localcpath + '*.csv')]
+    fileslist = [filename.rsplit('/', 1)[1] for filename in glob.glob(localpath + '*.csv')]
 
     client.command(f'CREATE DATABASE IF NOT EXISTS {dbname}')
 
@@ -38,17 +38,36 @@ def csv2clickhouse(localcpath, dbhost, dbport, dbname, dbuser, dbpassword, dbsec
         for i in range(len(columnType)):
             if columnType[i] == 'object':
                 columnType[i] = 'String'
-            # else:
-            #     columnType[i] = 'String'
-            elif columnType[i] == 'int64':
-                columnType[i] = 'UInt64'
+            elif columnType[i] == 'int8':
+                columnType[i] = 'Int8'
+            elif columnType[i] == 'int16':
+                columnType[i] = 'Int16'
             elif columnType[i] == 'int32':
+                columnType[i] = 'Int32'
+            elif columnType[i] == 'int64':
+                columnType[i] = 'Int64'
+            elif columnType[i] == 'uint8':
+                columnType[i] = 'UInt8'
+            elif columnType[i] == 'uint16':
+                columnType[i] = 'UInt16'
+            elif columnType[i] == 'uint32':
                 columnType[i] = 'UInt32'
+            elif columnType[i] == 'uint64':
+                columnType[i] = 'UInt64'            
+            elif columnType[i] == 'float16':
+                columnType[i] = 'Float32'
+            elif columnType[i] == 'float32':
+                columnType[i] = 'Float32'
             elif columnType[i] == 'float64':
                 columnType[i] = 'Float64'
-            elif columnType[i] == 'float32':
-                columnType[i] = 'Float32'    
-        
+
+            elif columnType[i] == 'bool':
+                columnType[i] = 'UInt8'
+            elif columnType[i] == 'datetime64ns':
+                columnType[i] = 'DateTime'
+            elif columnType[i] == 'timedeltans':
+                columnType[i] = 'Int64'     
+
         columnNameTypes = dict(zip(columnName, columnType))
         
         query = ''
@@ -57,29 +76,7 @@ def csv2clickhouse(localcpath, dbhost, dbport, dbname, dbuser, dbpassword, dbsec
         query = f'CREATE TABLE IF NOT EXISTS {dbname}.{filename[:-4]} ({query[:-2]}) ENGINE Memory'
         client.command(query)
 
-        if flag == 'EMPTY': # and len(client.command(f'SELECT * FROM {dbname}.{columnName[0]} LIMIT 1')) != 0:
-            # client.command(f'DELETE FROM {dbname}.{filename[:-4]} WHERE {columnName[0]} LIKE "%"')
-            query = f'ALTER "{filename[:-4]}" UPDATE _row_exists = 0 WHERE {columnName[0]} LIKE "%"'
-            print(query)
-            client.command(query)
-
-        # for row in df.iterrows():
-        #     print(row)
-
-        # for l in df.values.tolist():
-        #     # print(l)
-        #     s = ', '.join(f'"{str(e)}"' for e in l)
-        #     # print(s)
-        #     query = 'INSERT INTO OR IGNORE ' + dbname + '.' + filename[:-4] + ' VALUES (' + s + ')' 
-        #     print(query)
-        #     # client.command(f'INSERT INTO {dbname}.{filename[:-4]} VALUES ({s})')
-        #     client.query(query)
-
         client.insert_df(f'{dbname}.{filename[:-4]}', df)
-
-        # print()
-        # print(query)
-        # print()
 
         
 if __name__ == '__main__':
